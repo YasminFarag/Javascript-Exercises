4b06b568517c1fb61ad2164903d83a296d58be3f
yasmin <yasminefawzy22@gmail.com>
2019-03-18 13:51:44 +0100

    first exercise

---
+++ exercise1.js
@@ -0,0 +1,27 @@
+console.log('this is an exercise in console logging');
+
+
+var myValue = ('This is the value');
+console.log(myValue);
+
+
+var firstName =('Yasmin');
+var lastName =('Farag');
+console.log(firstName, lastName);
+
+var firstName =('claudia');
+var cityName =('NY');
+var jobTitle =('doctor');
+var age =('35');
+var car =('Mercedes');
+var children =('3');
+
+console.log(firstName, cityName, jobTitle, age, car, children);
+
+
+
+var familyName=('John Smith');
+var age=('43');
+var jobTitle=('teacher');
+var cityName=('berlin');
+console.log(familyName, 'is a '+ age + ' years old ', 'who lives in '+ cityName);
