8cbd5b7ec5de8e44b478136fd8b1aa4a3cf419af
yasmin <yasminefawzy22@gmail.com>
2019-03-19 14:18:51 +0100

    new updates

--- exercise1.js
+++ exercise1.js
@@ -27,5 +27,44 @@ var cityName=('berlin');
 var status =('married');
 console.log(familyName, 'is a '+ age + ' years old ', 'who lives in '+ cityName, ' he is ' + status);
 
+// data types exercises
+
+//1
+
+var firstString = 'yasminfarag';
+console.log(firstString);
+
+//2
+console.log(firstString[2] , firstString[6]);
+console.log();
+
+// 3 : 6
+
+var name =('John');
+var age = 30;
+var year = 2020;
+console.log(name + ' is ' + age + ' in ' + year);
+
+
+// 7,8
+
+let isMarried = 'true';
+console.log( name + ' is married: ' + isMarried);
+
+
+// 9
+
+let programmingLanguage ='JavaScript';
+let isFun = 'true';
+console.log(programmingLanguage + ' is fun: ' + isFun);
+
+// 10 
+
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+
+// 11
+
+console.log(programmingLanguage.length);
 
 
